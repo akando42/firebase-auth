a3777fb155265c970cc1922cf4888d86dfef31a9
Troy Do <user@iMac-cua-User.local>
2025-09-16 12:53:51 +0700

    login form

---
+++ README.md
@@ -0,0 +1,5 @@
+### Firebase Authentication
+
+[ ] Create New Account with Email
+[ ] Login Account with Email
+[ ] Display Firebase Data for Authenticated User
--- pages/index.js
+++ pages/index.js
@@ -6,21 +6,76 @@ import styles from '../styles/Home.module.css'
 export default class Home extends React.Component {
 	constructor(props){
 		super(props)
+
+		this.state = {
+			authenticated: false
+		}
+
+		this.authUser = this.authUser.bind(this)
 	}
 
-	async loadData(){
+	async authUser(){
+		this.setState({
+			authenticated: true
+		})
+	}
 
+	async logOut(){
+		this.setState({
+			authenticated: false
+		})
 	}
 
 	componentDidMount(){
-		this.loadData()
 	}
 
 	render(){
-		<div className={styles.container}>
-			<div className={styles.topNav}> 
-				Top Nav	
+		return(
+			<div className={styles.container}>
+				{
+					this.state.authenticated 
+					?	<div className={styles.topNav}> 
+							<div 
+								className={styles.profile}
+								onClick={this.logOut}
+							>
+
+							</div>
+						</div>
+					:   <div className={styles.authContainer}>
+							<div className={styles.usersList}>
+								Users
+							</div>
+							<div className={styles.authForm}>
+								<div className={styles.loginForm}>
+									<input 
+										className={styles.loginInput} 
+										type="email"
+									/>
+									<input 
+										className={styles.loginInput} 
+										type="password"
+									/>
+									<div className={styles.loginButton}>
+										Log in
+									</div>
+									<div  className={styles.forgotPassword} > 
+										Forgetten password ? 
+									</div>
+								</div>
+								<div>
+									<div 
+										className={styles.createAccount}
+										onClick={this.authUser}
+									> 
+										Create new account 
+									</div>
+								</div>
+							</div>
+						</div>
+				}
 			</div>
-		</div>
+		)
+		
 	}
 }
