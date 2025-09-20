d02193a98b3b90e3f956cf2420efb24e530268d3
Troy Do <user@iMac-cua-User.local>
2025-09-20 19:40:14 +0700

    Sign up and update Display Name

--- pages/index.js
+++ pages/index.js
@@ -3,21 +3,115 @@ import Head from "next/head";
 import Image from "next/image";
 import styles from '../styles/Home.module.css'
 
+import { initializeApp } from "firebase/app";
+import { 
+	TwitterAuthProvider, 
+	GithubAuthProvider, 
+	getAuth, 
+	signInWithPopup,
+	createUserWithEmailAndPassword,
+	signInWithEmailAndPassword, 
+	updateProfile
+} from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAiFHqtPYiH5IsCCUNHYBwiwyUFA_bqrQ4",
+  authDomain: "railbnb.firebaseapp.com",
+  projectId: "railbnb",
+  storageBucket: "railbnb.firebasestorage.app",
+  messagingSenderId: "1039689999605",
+  appId: "1:1039689999605:web:c3e4f247a7fb8af613d1bc",
+  measurementId: "G-WLBHVJHGW9"
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
 export default class Home extends React.Component {
 	constructor(props){
 		super(props)
 
 		this.state = {
-			authenticated: false
+			authenticated: false,
+			creatingUser: false, 
+			firstName: '',
+			lastName: '',
+			email: '',
+			password:'',
+			confirm_password: ''
 		}
 
-		this.authUser = this.authUser.bind(this)
+		this.createUser = this.createUser.bind(this)
+		this.startLogin = this.startLogin.bind(this)
+		this.setSignupFirstname = this.setSignupFirstname.bind(this)
+		this.setSignupLastname = this.setSignupLastname.bind(this)
+
+		this.setSignupEmail = this.setSignupEmail.bind(this)
+		this.setSignupPassword = this.setSignupPassword.bind(this)
+		this.setSignupConfirmPassword = this.setSignupConfirmPassword.bind(this)
+		this.signupNewUser = this.signupNewUser.bind(this)
 	}
 
-	async authUser(){
+	async createUser(){
 		this.setState({
-			authenticated: true
+			creatingUser: true
 		})
+	}
+
+	async startLogin(){
+		this.setState({
+			creatingUser: false
+		})
+	}
+
+	async setSignupFirstname(event){
+		this.setState({
+			firstName: event.target.value
+		})
+	}
+
+	async setSignupLastname(event){
+		this.setState({
+			lastName: event.target.value
+		})
+	}
+
+	async setSignupEmail(event){
+		this.setState({
+			email: event.target.value
+		})
+	}
+
+	async setSignupPassword(event){
+		this.setState({
+			password: event.target.value
+		})
+	}
+
+	async setSignupConfirmPassword(event){
+		this.setState({
+			confirm_password: event.target.value
+		})
+	}
+
+	async signupNewUser(){
+		let email = this.state.email
+		let password = this.state.password
+		let confirm_password = this.state.confirm_password
+		let name = `${this.state.firstName} ${this.state.lastName}`
+
+		if (password === confirm_password){
+			createUserWithEmailAndPassword(auth, email, password)
+				.then(async(userCredential) => {
+					const user = userCredential.user;
+					console.log("Firebase User ", user)
+					
+					updateProfile(auth.currentUser,{
+						displayName: name
+					})
+				})
+
+		}
 	}
 
 	async logOut(){
@@ -47,30 +141,86 @@ export default class Home extends React.Component {
 								Users
 							</div>
 							<div className={styles.authForm}>
-								<div className={styles.loginForm}>
-									<input 
-										className={styles.loginInput} 
-										type="email"
-									/>
-									<input 
-										className={styles.loginInput} 
-										type="password"
-									/>
-									<div className={styles.loginButton}>
-										Log in
-									</div>
-									<div  className={styles.forgotPassword} > 
-										Forgetten password ? 
-									</div>
-								</div>
-								<div>
-									<div 
-										className={styles.createAccount}
-										onClick={this.authUser}
-									> 
-										Create new account 
-									</div>
-								</div>
+								{
+									this.state.creatingUser 
+									?   <div className={styles.signupContainer}>
+											<div className={styles.signupForm}>
+												<div className={styles.nameInput}>
+													<input 
+														className={styles.firstName} 
+														placeholder="First Name"
+														onChange={this.setSignupFirstname}
+													/>
+													<input 
+														className={styles.lastName} 
+														placeholder="Last Name"
+														onChange={this.setSignupLastname}
+													/>
+												</div>
+												<input 
+													className={styles.signupInput} 
+													placeholder="Enter Your Email"
+													type="email"
+													onChange={this.setSignupEmail}
+												/>
+												<input 
+													className={styles.signupInput} 
+													placeholder="Enter Your Password"
+													type="password"
+													onChange={this.setSignupPassword}
+												/>
+												<input 
+													className={styles.signupInput} 
+													placeholder="Re-enter Your Password"
+													type="password"
+													onChange={this.setSignupConfirmPassword}
+												/>
+											</div>
+											<div 
+												className={styles.createAccount}
+												onClick={this.signupNewUser}
+											>
+												Sign Up 
+											</div>
+											<div  
+												className={styles.haveAccount} 
+												onClick={this.startLogin}
+											> 
+												Already Have An Account ? 
+											</div>
+										</div>
+									:   <div className={styles.loginContainer}>
+											<div className={styles.loginForm}>
+												<input 
+													className={styles.loginInput} 
+													type="email"
+													placeholder="Enter Your Email"
+													onChange={this.setLoginEmail}
+												/>
+												<input 
+													className={styles.loginInput} 
+													type="password"
+													placeholder="Enter Your Password"
+													onChange={this.setLoginEmail}
+												/>
+												<div className={styles.loginButton}>
+													Log in
+												</div>
+												<div  className={styles.forgotPassword} > 
+													Forgetten password ? 
+												</div>
+											</div>
+											<div 
+												className={styles.createAccount}
+												onClick={this.createUser}
+											> 
+												Create new account 
+											</div>
+										</div>
+									
+								}
+								
+								
 							</div>
 						</div>
 				}
